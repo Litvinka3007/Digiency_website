4fd001bc0b52c4539f52b1dc791d041e0ee5d9fa
Angelina <angelina16161616@gmail.com>
2021-10-04 14:31:00 +0300

    refactor: split the code into separate files

---
+++ #src/js/digital-counter.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function onScroll() {
+    let scrollNumbers = document.querySelectorAll(".our-experience-section__item-number");
+    scrollNumbers.forEach(function(item) {
+      let numberTop = item.getBoundingClientRect().top,
+          start = +item.innerHTML,
+          end = +item.dataset.max;
+      if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+        this.removeEventListener("scroll", onScroll);
+        var interval = setInterval(function () {
+          item.innerHTML = ++start;
+          if (start === end) {
+            clearInterval(interval);
+          }
+        }, 30);
+      }
+    });
+  });
+});
---
+++ #src/js/slider.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  $(".slider").slick({
+    arrows: false,
+    dots: false,
+    adaptiveHeight: true,
+    // autoplay: true,
+    speed: 300,
+    easing: 'linear',
+    initialSlide: 0,
+    infinite: true,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 10,
+    touchMove: true,
+    waitForAnimate: true,
+    centerMode: true,
+    variableWidth: true,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          // autoplay: true,
+          autoplaySpeed: 1000,
+          pauseOnFocus: true,
+          pauseOnHover: true,
+          pauseOnDotsHover: true,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+          centerMode: false,
+        }
+      }
+    ]
+  });
+});
