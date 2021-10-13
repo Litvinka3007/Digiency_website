3dbba322b6544ac3b7fc8c91d723679963b92d30
Angelina <angelina16161616@gmail.com>
2021-10-13 13:22:39 +0300

    fix: fix a bug of the slick-slider

--- #src/js/slider-testimonials.js
+++ #src/js/slider-testimonials.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     waitForAnimate: true,
     centerMode: false,
     centerPadding: '50px',
-    variableWidth: false,
+    variableWidth: true,
     mobileFirst: true,
     responsive: [
       {
@@ -25,7 +25,7 @@ $(document).ready(function() {
         settings: {
           dots: true,
           slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -33,7 +33,7 @@ $(document).ready(function() {
         settings: {
           dots: true,
           slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToScroll: 1
         }
       }
     ]
