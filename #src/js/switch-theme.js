c9a9d4e68070e1a0f1d71ff3a1f8e588384c8cdc
Angelina <angelina16161616@gmail.com>
2021-10-20 12:29:59 +0300

    feat: add saving a theme to local storage

--- #src/js/switch-theme.js
+++ #src/js/switch-theme.js
@@ -1,3 +1,7 @@
+if (!localStorage.theme) localStorage.theme = 'light';
+document.body.className = localStorage.theme;
+
 toggleThemeBtn.onclick = () => {
   document.body.classList.toggle('dark');
+  localStorage.theme = document.body.className || 'light';
 }
