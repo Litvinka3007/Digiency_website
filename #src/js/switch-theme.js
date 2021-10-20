9ea77cc9ba538b524a94a2421df01536fb4e3b9d
Angelina <angelina16161616@gmail.com>
2021-10-20 13:29:43 +0300

    feat: add a change of the images in the menu when changing the theme

--- #src/js/switch-theme.js
+++ #src/js/switch-theme.js
@@ -1,7 +1,24 @@
 if (!localStorage.theme) localStorage.theme = 'light';
 document.body.className = localStorage.theme;
 
+if (document.body.classList.contains('dark')) {
+  document.getElementById('logo').src = './img/logo_white.svg';
+  document.getElementById('theme-icon').src = './img/icons/light-theme.svg';
+} else {
+  document.getElementById('logo').src = './img/logo.svg';
+  document.getElementById('theme-icon').src = './img/icons/dark-theme.svg';
+}
+
 toggleThemeBtn.onclick = () => {
   document.body.classList.toggle('dark');
+
+  if (document.body.classList.contains('dark')) {
+    document.getElementById('logo').src = './img/logo_white.svg';
+    document.getElementById('theme-icon').src = './img/icons/light-theme.svg';
+  } else {
+    document.getElementById('logo').src = './img/logo.svg';
+    document.getElementById('theme-icon').src = './img/icons/dark-theme.svg';
+  }
+
   localStorage.theme = document.body.className || 'light';
 }
