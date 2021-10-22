f188c60b72dde2292305cdf558e8ab2f63855489
Angelina <angelina16161616@gmail.com>
2021-10-22 15:45:36 +0300

    add changes

--- #src/js/switch-theme.js
+++ #src/js/switch-theme.js
@@ -4,9 +4,15 @@ document.body.className = localStorage.theme;
 if (document.body.classList.contains('dark')) {
   document.getElementById('logo').src = './img/logo_white.svg';
   document.getElementById('theme-icon').src = './img/icons/light-theme.svg';
+  document.getElementById('insta-main-icon').src = './img/icons/socials/dark-mode/instagram.svg';
+  document.getElementById('twitter-main-icon').src = './img/icons/socials/dark-mode/twitter.svg';
+  document.getElementById('skype-main-icon').src = './img/icons/socials/dark-mode/skype.svg';
 } else {
   document.getElementById('logo').src = './img/logo.svg';
   document.getElementById('theme-icon').src = './img/icons/dark-theme.svg';
+  document.getElementById('insta-main-icon').src = './img/icons/socials/instagram.png';
+  document.getElementById('twitter-main-icon').src = './img/icons/socials/twitter.png';
+  document.getElementById('skype-main-icon').src = './img/icons/socials/skype.png';
 }
 
 toggleThemeBtn.onclick = () => {
@@ -15,9 +21,15 @@ toggleThemeBtn.onclick = () => {
   if (document.body.classList.contains('dark')) {
     document.getElementById('logo').src = './img/logo_white.svg';
     document.getElementById('theme-icon').src = './img/icons/light-theme.svg';
+    document.getElementById('insta-main-icon').src = './img/icons/socials/dark-mode/instagram.svg';
+    document.getElementById('twitter-main-icon').src = './img/icons/socials/dark-mode/twitter.svg';
+    document.getElementById('skype-main-icon').src = './img/icons/socials/dark-mode/skype.svg';
   } else {
     document.getElementById('logo').src = './img/logo.svg';
     document.getElementById('theme-icon').src = './img/icons/dark-theme.svg';
+    document.getElementById('insta-main-icon').src = './img/icons/socials/instagram.png';
+    document.getElementById('twitter-main-icon').src = './img/icons/socials/twitter.png';
+    document.getElementById('skype-main-icon').src = './img/icons/socials/skype.png';
   }
 
   localStorage.theme = document.body.className || 'light';
