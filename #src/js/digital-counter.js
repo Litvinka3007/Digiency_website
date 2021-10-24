fda430a5c2f73ce763b3ee89d95d50dfff0d4e57
Angelina <angelina16161616@gmail.com>
2021-10-24 22:32:11 +0300

    fix: change the script for the blocks with numbers counting up from 0

--- #src/js/digital-counter.js
+++ #src/js/digital-counter.js
@@ -1,19 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function onScroll() {
-    let scrollNumbers = document.querySelectorAll(".our-experience-section__item-number");
-    scrollNumbers.forEach(function(item) {
-      let numberTop = item.getBoundingClientRect().top,
-          start = +item.innerHTML,
-          end = +item.dataset.max;
-      if (window.pageYOffset > numberTop - window.innerHeight / 2) {
-        this.removeEventListener("scroll", onScroll);
-        let interval = setInterval(function () {
-          item.innerHTML = ++start;
-          if (start === end) {
-            clearInterval(interval);
-          }
-        }, 30);
-      }
-    });
+$(document).ready(function(){
+  $(".our-experience-section__item-number").counterUp({
+    delay: 10,
+    time: 1200
   });
 });
